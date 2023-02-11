d0398b821d0ec800a296278df6f16dabbcccefaf
Андрей <honor10love2@gmail.com>
2023-02-11 17:01:43 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        int menu = scanner.nextInt();
+
+        System.out.println("Выберите пункт меню: ");
+        switch (menu){
+            case 1;
+
+
+        }
+    }
+
+    public void printMenu(){
+        System.out.println("");
+    }
+}
